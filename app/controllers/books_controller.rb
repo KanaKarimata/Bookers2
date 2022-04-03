030baf9aa7bc47de9bade6cff0d20d182a624c20
KanaKarimata <kana.lt22@gmail.com>
2022-04-03 12:20:49 +0000

    作成中：編集画面まで作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,12 +3,38 @@ class BooksController < ApplicationController
     @book = Book.new
     @books = Book.all
   end
-  
-  
+
+  def create
+    book = Book.new(books_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(books_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
